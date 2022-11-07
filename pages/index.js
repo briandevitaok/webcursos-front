bbedd39fcc083339782e625b825b2f256ab7298e
unknown <16tupac16@gmail.com>
2022-11-06 21:36:36 -0300

    CAMBIOS

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-          <h1>🚀Mi web de cursos</h1>
+          <h1>🚀Mi web de cursos - Marge </h1>
     </div>
   )
 }
