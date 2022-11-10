5706933478b0534c82314dd2ac6977b7c1df04e4
unknown <16tupac16@gmail.com>
2022-11-10 11:41:35 -0300

    Cambios

--- pages/index.js
+++ pages/index.js
@@ -28,11 +28,12 @@ console.log({cours})
     <h1>🚀 Forza Cursos </h1>
     {
       cours.map( cours => (
-        <div className='df fdc' key={cours.id} style={{width:'50rem'}}>
+        <div className='df fdc' key={cours._id} style={{width:'50rem'}}>
         <div className='df jcsb'  style={{width:'50rem',background:'white',padding:'0.75rem', borderRadius:'0.5rem'}}>
           <div style={{boxShadow:'0 2px 5px rgba(0,0,0,0.2)', marginRight:'1rem'}}>
         <Image
          src={cours.thumbnail}
+         alt={cours.name}
          width={150 * 1.77}
          height={150}
         />
